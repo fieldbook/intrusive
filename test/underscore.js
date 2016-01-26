7d5385e33639bf33867a689b44e13c5e2d5ab078
Ben Bernard <github@benjaminbernard.com>
2016-01-25 23:32:49 -0800

    Add some more tests for underscore

--- test/underscore.js
+++ test/underscore.js
@@ -29,4 +29,95 @@ describe('Underscore mixins', function () {
       return expect(addArray).deep.equal({foo: {bar: {baz: []}}, qux: 1});
     })
   })
+
+  describe('soak', function () {
+    var obj = {
+      nested: {
+        deep: 1,
+        fn: function (arg) { return arg },
+        get: function (key) {
+          if (key === 'passed') return true;
+          return false;
+        },
+        getHash: function () {
+          return {another: {nested: 3}};
+        },
+      }
+    };
+
+    it('should return null for a nonexistant path', function () {
+      expect(obj._.soak('nested.nope.bar')).to.be.undefined;
+    })
+
+    it('should return the correct value for a nested path', function () {
+      return expect(obj._.soak('nested.deep')).equal(1);
+    })
+
+    it('should call a function with the passed arg', function () {
+      return expect(obj._.soak('nested.fn', 2)).equal(2)
+    })
+
+    it('should call a getter', function () {
+      return expect(obj._.soak('nested.@passed')).true;
+    })
+
+    it('should call an intermediate function', function () {
+      return expect(obj._.soak('nested.getHash().another.nested')).to.equal(3);
+    })
+  })
+
+  describe('compactNullish', function () {
+    it('should remove null and undefined', function () {
+      return expect(_.compactNullish(['', true, undefined, false, null, 0, 1, 'foo'])).to.deep.equal(['', true, false, 0, 1, 'foo'])
+    })
+  })
+
+  describe('deepClone', function () {
+    var obj = {foo: ['bar']};
+    it('should return a the correct data', function () {
+      return expect(obj._.deepClone()).deep.equal({foo: ['bar']});
+    })
+
+    it('should not reflect modifications after cloning', function () {
+      var copy = obj._.deepClone();
+      obj.foo[0] = 'zip;'
+      return expect(copy).deep.equal({foo: ['bar']});
+    })
+  })
+
+  describe('concat', function () {
+    it('should concat obj and arrays together', function () {
+      return expect(_.concat([1, 2], 3, [4, 5])).deep.equal([1, 2, 3, 4, 5]);
+    })
+  })
+
+  describe('pluckSoak', function () {
+    var array = [
+      {nested: {foo: 1}},
+      {},
+      {nested: {bar: 1}}
+    ];
+    it('should do soak on each element', function () {
+      return expect(array._.pluckSoak('nested.foo')).deep.equal([1, undefined, undefined])
+    })
+  })
+
+  describe('mapMethod', function () {
+    var obj = {
+      bar: 2,
+      meth: function (item) {
+        return this.bar + item; // jscs: nestedThisOk
+      },
+    }
+
+    it('shoudl call the method with correct this', function () {
+      return expect(_.mapMethod([1, 2], obj, 'meth')).deep.equal([3, 4])
+    })
+  })
+
+  describe('stencil', function () {
+    it('should replace data correctly', function () {
+      return expect('foo {{name}}'.stencil({name: 'bar'})).equal('foo bar');
+    })
+  })
 })
