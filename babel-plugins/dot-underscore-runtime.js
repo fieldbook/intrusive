d82c5b5681dc72015ffa09db70b5cc7bcea65301
Ada Cohen <spencer@fieldbook.com>
2016-06-11 18:26:35 -0700

    Add babel plugin for dot underscore

---
+++ babel-plugins/dot-underscore-runtime.js
@@ -0,0 +1,10 @@
+// Runtime function for dot-underscore babel plugin.
+
+// Defines the global _getUnderscore which, when passed an argument, will
+// check if that argument has an '_' property (to maximize compatibility), and
+// return it if so. If the property does not exist, returns an underscore
+// wrapper for the object.
+global._getUnderscore = function (obj) {
+  // The `in` operator can only be used with objects
+  return (typeof obj === 'object' && '_' in obj) ? obj['_'] : _(obj);
+}
--- test/init.js
+++ test/init.js
@@ -1,5 +1,28 @@
 var chai = require('chai');
+var path = require('path');
 
 chai.use(require('chai-as-promised'));
 
 global.expect = chai.expect;
+
+var rootdir = path.resolve(__dirname, '..');
+
+// Returns a path relative to the project root, no matter what file it's called from.
+var projpath = function (relativePath) {
+  return path.join(rootdir, relativePath);
+}
+
+// First require in the babel hook
+require('babel-register')({
+  // Do not babel generated files or external files
+  ignore: function (filename) {
+    return (!/test\/dot-underscore/.test(filename))
+
+    return false;
+  },
+  compact: false,
+  plugins: [
+    "transform-es2015-block-scoping",
+    projpath("babel-plugins/dot-underscore"),
+  ],
+});
